d74446380ddc9bd0f83fb1bd1ab2ac0da0624f3e
kahnwong <karn@karnwong.me>
2023-04-06 01:53:16 +0700

    add experiment_spark.py

--- README.md
+++ README.md
@@ -2,20 +2,30 @@
 
 ## Frameworks Used
 
-<!-- [TODO] - add links -->
-
-- pandas
+- ~~pandas~~ (not present in the experiment, because it's very unrealistic to expect pandas to be able to open 15GB data on a 16GB RAM machine)
 - polars
-- spark (single-node)
 - duckdb
+- spark (single-node)
 
 ## Experiments
 
--
+query: filter by percentiles + groupby
+
+| framework | mode      |
+| --------- | --------- |
+| polars    | default   |
+| duckdb    | default   |
+| spark     | default   |
+| spark     | optimized |
 
 ## Data source
 
-See [here](src/utils/download_dataset.sh)
+See [here](src/utils/download_dataset.sh).
+
+- total size: `15 GB`,
+- total records: `1,195,313,202 - around 1200 million rows`
+- partitions: `year 2012` to `year 2022` (older partitions have different schema)
+- **dirty data**: some columns have mismatched data types across partitions
 
 ## Usage
 
---
+++ src/experiment_spark.py
@@ -0,0 +1,101 @@
+import json
+import logging as log
+import time
+import uuid
+
+import pyspark.sql.functions as F
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+from pyspark.sql.window import Window
+
+log.basicConfig(format="%(asctime)s - [%(levelname)s] %(message)s", level=log.INFO)
+
+################## metadata ##################
+ENGINE = "spark"
+MODE = "default"
+run_id = str(uuid.uuid4())
+
+log.info(f"start run - {ENGINE}-{MODE}: {run_id}")
+################## init ##################
+
+spark = (
+    SparkSession.builder.appName("Dataframe Frameworks Showdown")
+    .config("spark.executor.memory", "16g")
+    .config("spark.driver.memory", "16g")
+    .getOrCreate()
+)
+
+start_time = time.time()  # start timer
+
+
+################## main ##################
+df = spark.read.parquet("data/nyc-trip-data")
+
+grains = ["year", "month"]
+
+w = Window().partitionBy(grains).orderBy("trip_length_minute")
+
+df_out = (
+    df
+    #### create trip_length_minute ####
+    .withColumn(
+        "tpep_pickup_datetime",
+        F.to_timestamp(col("tpep_pickup_datetime"), "yyyy-MM-dd HH:mm:ss"),
+    )
+    .withColumn(
+        "tpep_dropoff_datetime",
+        F.to_timestamp(col("tpep_dropoff_datetime"), "yyyy-MM-dd HH:mm:ss"),
+    )
+    .withColumn(
+        "trip_length_minute",
+        (
+            F.unix_timestamp(col("tpep_dropoff_datetime"))
+            - F.unix_timestamp(col("tpep_pickup_datetime"))
+        )
+        / 60,
+    )
+    #### filter by percentile ####
+    .withColumn("trip_length_minute_percentile", F.percent_rank().over(w))
+    .where(col("trip_length_minute_percentile").between(0.2, 0.8))
+    #### aggregate ####
+    .groupBy(
+        *grains
+        + [
+            "VendorID",
+            "payment_type",
+        ]
+    )
+    .agg(
+        # passenger_count
+        F.min(col("passenger_count")).alias("min_passenger_count"),
+        F.max(col("passenger_count")).alias("max_passenger_count"),
+        F.avg(col("passenger_count")).alias("avg_passenger_count"),
+        # trip_distance
+        F.min(col("trip_distance")).alias("min_trip_distance"),
+        F.max(col("trip_distance")).alias("max_trip_distance"),
+        F.avg(col("trip_distance")).alias("avg_trip_distance"),
+        # total_amount
+        F.min(col("total_amount")).alias("min_total_amount"),
+        F.max(col("total_amount")).alias("max_total_amount"),
+        F.avg(col("total_amount")).alias("avg_total_amount"),
+    )
+)
+
+log.info(f"output rows: {df_out.count()}")  # trigger spark actions
+end_time = time.time()  # end timer
+
+elapsed_time = end_time - start_time
+log.info(f"Elapsed time was {elapsed_time} seconds")
+
+################## logging ##################
+with open("data/runs.json", "a") as f:
+    r = {
+        "engine": ENGINE,
+        "mode": MODE,
+        "duration": elapsed_time,
+        "processed_rows": df.count(),
+        "uuid": run_id,
+    }
+
+    f.write(json.dumps(r))
+    f.write("\n")
